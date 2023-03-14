4ab7e5400efe4f610e3482747e95a0bf1dff9d03
test <novalzf29@gmail.com>
2023-03-14 11:13:17 +0700

    test

---
+++ resources/views/datadiri.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>DATA DIRI SAYA</title>
+</head>
+
+<body class="card">
+    <img src="img/trpl.png" alt="trpl">
+    <h1>|--------------------------------------------------|</h1>
+    <h1>NAMA : NOVAL ZAHWAN FHALMA</h1>
+    <h1>NIM: 4342201002</h1>
+    <h1>|--------------------------------------------------|</h1>
+</body>
+
+</html>
